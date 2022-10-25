f491e23ab12ea6ce46f69622421a3bd2ac458c61
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-10-25 20:58:38 +0200

    add body middleware validation

---
+++ common/middleware/body.validation.middleware.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { validationResult } from 'express-validator';
+
+
+class BodyValidationMiddleware {
+    verifyBodyFieldErrors(req: express.Request, res: any, next: express.NextFunction) {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty) {
+
+            return res.status(400).send({ errors: errors.array() });
+        }
+
+    }
+}
